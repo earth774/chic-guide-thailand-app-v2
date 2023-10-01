33df4c844a249b1b27e60858a8ad40a63a6f0d25
earth774 <sutthiphongnuanma@gmail.com>
2023-10-01 16:47:53 +0700

    update code

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,31 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'chic-guide-th',
+  appId: 'com.chicguide.thailand',
+  appName: 'Chic Guide Thailand',
   webDir: 'www',
-  server: {
-    androidScheme: 'https'
+  bundledWebRuntime: false,
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: '#ffffffff',
+      androidSplashResourceName: 'splash',
+      androidScaleType: 'CENTER_CROP',
+      showSpinner: false,
+      androidSpinnerStyle: 'large',
+      iosSpinnerStyle: 'small',
+      spinnerColor: '#999999',
+      splashFullScreen: true,
+      splashImmersive: true,
+    },
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      iosClientId: '969989942144-jdk8e8uefi2bvc0rq7ciooarklj4no3s.apps.googleusercontent.com',
+      androidClientId: '427617387136-dp3kohgeo74hsrvjkjc7v9207c8k41jh.apps.googleusercontent.com',
+      serverClientId: '427617387136-dp3kohgeo74hsrvjkjc7v9207c8k41jh.apps.googleusercontent.com',
+      forceCodeForRefreshToken: true
+    }
   }
 };
 
